7e12eb59dc71f00ec6cc15dc262363e123df37f1
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-09-09 12:50:02 +0200

    Testing: completed the test code to make the failure reason explicit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "TripService/TripService.h"
 #include "TripService/User.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
 
-#define A_USER_ID 1
+#define FIRST_USER_ID 1
+#define SECOND_USER_ID 2
 
 class TestableTripService : public TripService {
 public:
@@ -22,9 +24,37 @@ TEST(TripServiceShould, validate_the_logged_in_user)
     EXPECT_THROW(tripService->GetTripsByUser(nullptr), const char *);
 }
 
-TEST(TripServiceShould, do_something_with_logged_in_user)
+TEST(TripServiceShould, return_no_trips_when_users_are_not_friends)
 {
-    User *loggedUser = new User(A_USER_ID);
+    User *loggedUser = new User(FIRST_USER_ID);
+    User *anotherUser = new User(SECOND_USER_ID);
+
+    auto tripToBrazil = Trip("Brazil");
+    anotherUser->AddTrip(tripToBrazil);
+    auto tripToLondon = Trip("London");
+    anotherUser->AddTrip(tripToLondon);
+
     auto *tripService = new TestableTripService(loggedUser);
-    EXPECT_THROW(tripService->GetTripsByUser(nullptr), const char *);
+    auto trips = tripService->GetTripsByUser(anotherUser);
+    EXPECT_TRUE(trips.empty());
+}
+
+TEST(TripServiceShould, return_trips_when_users_are_friends)
+{
+    User *loggedUser = new User(FIRST_USER_ID);
+    User *anotherUser = new User(SECOND_USER_ID);
+
+    auto tripToBrazil = Trip("Brazil");
+    anotherUser->AddTrip(tripToBrazil);
+    auto tripToLondon = Trip("London");
+    anotherUser->AddTrip(tripToLondon);
+    anotherUser->AddFriend(*loggedUser);
+
+    auto *tripService = new TestableTripService(loggedUser);
+    try {
+        auto trips = tripService->GetTripsByUser(anotherUser);
+        EXPECT_THAT(trips, ::testing::ElementsAre(tripToLondon, tripToBrazil));
+    } catch (const char * exception) {
+        FAIL() << exception;
+    }
 }

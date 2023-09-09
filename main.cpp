0e4fb43fc92fc53838df544a8fc6c589fbf9d190
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-09-09 12:18:55 +0200

    Testing: created a new generic test to explore the behaviour when there's a logged in user

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "TripService/User.h"
 #include <gtest/gtest.h>
 
+#define A_USER_ID 1
+
 class TestableTripService : public TripService {
 public:
     explicit TestableTripService(User *loggedUser) : loggedUser(loggedUser) {}
@@ -17,5 +19,12 @@ TEST(TripServiceShould, validate_the_logged_in_user)
 {
     User *notLoggedUser = nullptr;
     auto *tripService = new TestableTripService(notLoggedUser);
+    EXPECT_THROW(tripService->GetTripsByUser(nullptr), const char *);
+}
+
+TEST(TripServiceShould, do_something_with_logged_in_user)
+{
+    User *loggedUser = new User(A_USER_ID);
+    auto *tripService = new TestableTripService(loggedUser);
     EXPECT_THROW(tripService->GetTripsByUser(nullptr), const char *);
 }

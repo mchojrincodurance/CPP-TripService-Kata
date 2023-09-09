5cead571f9cd81db078bf140bca1ace3e4f94bf4
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-09-09 12:35:59 +0200

    Added methods to compare trips in order to leverage gtest matchers

--- TripService/Trip.h
+++ TripService/Trip.h
@@ -3,6 +3,19 @@
 
 class Trip
 {
+public:
+    explicit Trip(const char *destination) : destination(destination) {}
+
+    bool operator==(const Trip &rhs) const {
+        return destination == rhs.destination;
+    }
+
+    bool operator!=(const Trip &rhs) const {
+        return !(rhs == *this);
+    }
+
+private:
+    const char * destination;
 };
 
 #endif
